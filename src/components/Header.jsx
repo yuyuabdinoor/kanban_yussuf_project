212e9e47a431e83ba593602373f9968959713c35
yuyuabdinoor <yuyuabdinoor@gmail.com>
2024-01-08 15:31:10 +0300

    Setting Up environment

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+
+function Header() {
+  return (
+    <div className=' p-4 fixed left-0 bg-white dark:bg-[#2b2c37] z-50 right-0'>
+        <header className=' flex justify-between dark:text-white items-center'></header>  
+         <div>
+            {/*Left Side}
+         </div>
+         
+         
+         
+         
+         
+         
+    </div>
+  )
+}
+
+export default Header
